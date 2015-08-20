46340f1b27f207968885eece1a37944ba0032528
Fadi Alnabolsi <fadi@service.com>
2015-08-20 14:37:11 -0400

    ch 11 commit 3, rename all item input ids and names. still broken

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -11,7 +11,7 @@ class ItemFormTest(TestCase):
 		self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
 
 
-	def test_form_renders_item_text_input(self):
+	def test_form_renders_text_input(self):
 		form = ItemForm()
 		print(form.as_p())
 		self.assertIn('placeholder="Enter a to-do item"', form.as_p())
